733327d8c02234bf201a8ba118ed3a582c6caf11
Mathewclementina <105178606+Mathewclementina@users.noreply.github.com>
2022-11-16 09:24:33 +0100

    Update env2.c

--- env2.c
+++ env2.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * copy_info - copies info to create
+ * copy_info - copy information to create
  * a new env or alias
  * @name: name (env or alias)
  * @value: value (env or alias)
- *
  * Return: new env or alias.
  */
 char *copy_info(char *name, char *value)
@@ -26,8 +25,7 @@ char *copy_info(char *name, char *value)
 }
 
 /**
- * set_env - sets an environment variable
- *
+ * set_env - set an environment variable
  * @name: name of the environment variable
  * @value: value of the environment variable
  * @datash: data structure (environ)
@@ -58,11 +56,10 @@ void set_env(char *name, char *value, data_shell *datash)
 }
 
 /**
- * _setenv - compares env variables names
- * with the name passed.
+ * _setenv - compare env variables names
+ * with the name passed
  * @datash: data relevant (env name and env value)
- *
- * Return: 1 on success.
+ * Return: 1 on success
  */
 int _setenv(data_shell *datash)
 {
@@ -79,10 +76,8 @@ int _setenv(data_shell *datash)
 }
 
 /**
- * _unsetenv - deletes a environment variable
- *
+ * _unsetenv - delete a environment variable
  * @datash: data relevant (env name)
- *
  * Return: 1 on success.
  */
 int _unsetenv(data_shell *datash)
