8b039bc896563b9c159a8c79e96addb8e80acce5
Mathewclementina <105178606+Mathewclementina@users.noreply.github.com>
2022-11-16 09:15:46 +0100

    Update shell_loop.c

--- shell_loop.c
+++ shell_loop.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * without_comment - deletes comments from the input
- *
+ * without_comment - delete comments from the input
  * @in: input string
  * Return: input without comments
  */
@@ -38,7 +37,6 @@ char *without_comment(char *in)
 /**
  * shell_loop - Loop of shell
  * @datash: data relevant (av, input, args)
- *
  * Return: no return.
  */
 void shell_loop(data_shell *datash)
