0629f1702a213d592f25c3ebd8fc3123c9df1a0c
Mathewclementina <105178606+Mathewclementina@users.noreply.github.com>
2022-11-16 09:18:29 +0100

    Update aux_error2.c

--- aux_error2.c
+++ aux_error2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * error_env - error message for env in get_env.
+ * error_env - error message for env in get_env
  * @datash: data relevant (counter, arguments)
  * Return: error message.
  */
@@ -36,9 +36,8 @@ char *error_env(data_shell *datash)
 	return (error);
 }
 /**
- * error_path_126 - error message for path and failure denied permission.
- * @datash: data relevant (counter, arguments).
- *
+ * error_path_126 - error message for path and failure denied permission
+ * @datash: data relevant (counter, arguments)
  * Return: The error string.
  */
 char *error_path_126(data_shell *datash)
